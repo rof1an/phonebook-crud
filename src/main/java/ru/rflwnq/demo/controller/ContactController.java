31622079d83914a936151ceac8fe93e314ce3fe5
rof1an <egorrachkov0@gmail.com>
2024-01-22 20:10:58 +0500

    add get phone logic

---
+++ src/main/java/ru/rflwnq/demo/controller/ContactController.java
@@ -0,0 +1,29 @@
+package ru.rflwnq.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.rflwnq.demo.dto.ContactDto;
+import ru.rflwnq.demo.entity.Contact;
+import ru.rflwnq.demo.service.ContactService;
+import ru.rflwnq.demo.util.ContactMapper;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contacts")
+public class ContactController {
+    private final ContactService contactService;
+    private final ContactMapper mapper;
+
+    @Autowired
+    public ContactController(ContactService contactService, ContactMapper mapper) {
+        this.contactService = contactService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public List<ContactDto> getAllContacts() {
+        List<Contact> allContacts = contactService.getAllContacts();
+        return mapper.toDtoList(allContacts);
+    }
+}
